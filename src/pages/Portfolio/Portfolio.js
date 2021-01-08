3d279bc7b7dacd118d2f10252bdbf2b5affe8667
Greg Samuelson <grssam@gmail.com>
2021-01-07 16:46:24 -0800

    portfolio cards

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,5 +1,25 @@
-import Card from '../../components/Card'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
+import Crd from '../../components/Crd'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
 const portfolioInfo = [
   {
@@ -46,15 +66,46 @@ const portfolioInfo = [
 
 
 const Portfolio = () => {
-  
+
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
+
+  const handleChange = (event) => {
+    setSpacing(Number(event.target.value));
+  };
+
+
   return (
-    <>
-    <h1>Portfolio Page</h1>
-    {
+
+  //   <Grid container className={classes.root} spacing={2}>
+  //     <Grid item xs={12}>
+  //       <Grid container justify="center" spacing={spacing}>
+  //         {[0, 1, 2].map((value) => (
+  //           <Grid key={value} item>
+  //             <Paper className={classes.paper} />
+  //           </Grid>
+  //         ))}
+  //       </Grid>
+  //     </Grid>
+  //   </Grid>
+  // );
+
+<>
+  <h1>Portfolio Page</h1>
+
+
+  {
     portfolioInfo.map((data, i) =>
-      <Card/>)
-    }
-    </>
+      <Crd
+        img={data.img}
+        name={data.name}
+        text={data.text}
+        link={data.link}
+        github={data.github}
+        cardType={data.cardType}
+      />)
+  }
+  </>
   )
 }
 
